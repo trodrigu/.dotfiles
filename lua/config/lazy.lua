122377b30e98dfa03a511af7d3cc90b4118fe724
Thomas Rodriguez <thomas.rodriguez@recordedfuture.com>
2025-07-06 15:50:31 -0600

    add lazy bootstrap and config

---
+++ lua/config/lazy.lua
@@ -0,0 +1,335 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        if opts.desc then
+            opts.desc = "keymaps.lua: " .. opts.desc
+        end
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+require("lazy").setup({
+  spec = {
+    { "elixir-tools/elixir-tools.nvim", dependencies = {"nvim-lua/plenary.nvim"}},
+    {"neovim/nvim-lspconfig"},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"hrsh7th/cmp-buffer"},
+    {"hrsh7th/nvim-cmp"},
+    {"hrsh7th/cmp-vsnip"},
+    {"L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp"},
+    {"saadparwaiz1/cmp_luasnip"},
+    {"overcache/NeoSolarized"},
+    {"jeffkreeftmeijer/vim-numbertoggle"},
+    {"kana/vim-textobj-user"},
+    {"tpope/vim-abolish"},
+    {"tpope/vim-surround"},
+    {"tpope/vim-fugitive"},
+    {"jeetsukumaran/vim-indentwise"},
+    {"michaeljsmith/vim-indent-object"},
+    {"scrooloose/nerdcommenter"},
+    {"godlygeek/tabular"},
+    {"nvim-treesitter/nvim-treesitter"},
+    {"nvim-treesitter/nvim-treesitter-textobjects"},
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
+    },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"windwp/nvim-spectre"},
+    {"vim-test/vim-test"},
+    {"tpope/vim-repeat"},
+    {"ggandor/lightspeed.nvim"},
+    {"kdheepak/lazygit.nvim"},
+    {"folke/zen-mode.nvim"},
+    {"rust-lang/rust.vim"},
+    {"rafamadriz/friendly-snippets"},
+    --{"andyl/vim-textobj-elixir"},
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
+    { "morhetz/gruvbox" },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+    {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "j-hui/fidget.nvim",
+        opts = {},
+      },
+      {
+        "mfussenegger/nvim-dap",
+        config = function(self, opts)
+          -- Debug settings if you're using nvim-dap
+          local dap = require("dap")
+
+          dap.configurations.scala = {
+            {
+              type = "scala",
+              request = "launch",
+              name = "RunOrTest",
+              metals = {
+                runType = "runOrTestFile",
+                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+              },
+            },
+            {
+              type = "scala",
+              request = "launch",
+              name = "Test Target",
+              metals = {
+                runType = "testTarget",
+              },
+            },
+          }
+        end
+      },
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+
+      -- Example of settings
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+        gradleScript = "/Users/thomas/Code/main/gradlew"
+      }
+
+      -- *READ THIS*
+      -- I *highly* recommend setting statusBarProvider to either "off" or "on"
+      --
+      -- "off" will enable LSP progress notifications by Metals and you'll need
+      -- to ensure you have a plugin like fidget.nvim installed to handle them.
+      --
+      -- "on" will enable the custom Metals status extension and you *have* to have
+      -- a have settings to capture this in your statusline or else you'll not see
+      -- any messages from metals. There is more info in the help docs about this
+      metals_config.init_options.statusBarProvider = "off"
+
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      metals_config.on_attach = function(client, bufnr)
+        require("metals").setup_dap()
+
+        -- LSP mappings
+        map("n", "gD", vim.lsp.buf.definition)
+        map("n", "K", vim.lsp.buf.hover)
+        map("n", "gi", vim.lsp.buf.implementation)
+        map("n", "gr", vim.lsp.buf.references)
+        map("n", "gds", vim.lsp.buf.document_symbol)
+        map("n", "gws", vim.lsp.buf.workspace_symbol)
+        map("n", "<leader>cl", vim.lsp.codelens.run)
+        map("n", "<leader>sh", vim.lsp.buf.signature_help)
+        map("n", "<leader>rn", vim.lsp.buf.rename)
+        map("n", "<leader>f", vim.lsp.buf.format)
+        map("n", "<leader>ca", vim.lsp.buf.code_action)
+
+        map("n", "<leader>ws", function()
+          require("metals").hover_worksheet()
+        end)
+
+        -- all workspace diagnostics
+        map("n", "<leader>aa", vim.diagnostic.setqflist)
+
+        -- all workspace errors
+        map("n", "<leader>ae", function()
+          vim.diagnostic.setqflist({ severity = "E" })
+        end)
+
+        -- all workspace warnings
+        map("n", "<leader>aw", function()
+          vim.diagnostic.setqflist({ severity = "W" })
+        end)
+
+        -- buffer diagnostics only
+        map("n", "<leader>d", vim.diagnostic.setloclist)
+
+        map("n", "[c", function()
+          vim.diagnostic.goto_prev({ wrap = false })
+        end)
+
+        map("n", "]c", function()
+          vim.diagnostic.goto_next({ wrap = false })
+        end)
+
+        -- Example mappings for usage with nvim-dap. If you don't use that, you can
+        -- skip these
+        map("n", "<leader>dc", function()
+          require("dap").continue()
+        end)
+
+        map("n", "<leader>dr", function()
+          require("dap").repl.toggle()
+        end)
+
+        map("n", "<leader>dK", function()
+          require("dap.ui.widgets").hover()
+        end)
+
+        map("n", "<leader>dt", function()
+          require("dap").toggle_breakpoint()
+        end)
+
+        map("n", "<leader>dso", function()
+          require("dap").step_over()
+        end)
+
+        map("n", "<leader>dsi", function()
+          require("dap").step_into()
+        end)
+
+        map("n", "<leader>dl", function()
+          require("dap").run_last()
+        end)
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
+
+  },
+  {
+    "olimorris/codecompanion.nvim", -- The KING of AI programming
+    dependencies = {
+      "j-hui/fidget.nvim",
+      -- { "echasnovski/mini.pick", config = true },
+      -- { "ibhagwan/fzf-lua", config = true },
+    },
+    --init = function()
+      --vim.cmd([[cab cc CodeCompanion]])
+      --require("legendary").keymaps({
+        --{
+          --itemgroup = "CodeCompanion",
+          --icon = "",
+          --description = "Use the power of AI...",
+          --keymaps = {
+            --{
+              --"<C-a>",
+              --"<cmd>CodeCompanionActions<CR>",
+              --description = "Open the action palette",
+              --mode = { "n", "v" },
+            --},
+            --{
+              --"<LocalLeader>a",
+              --"<cmd>CodeCompanionChat Toggle<CR>",
+              --description = "Toggle a chat buffer",
+              --mode = { "n", "v" },
+            --},
+            --{
+              --"ga",
+              --"<cmd>CodeCompanionChat Add<CR>",
+              --description = "Add selected text to a chat buffer",
+              --mode = { "n", "v" },
+            --},
+          --},
+        --},
+      --})
+      --require("plugins.custom.spinner"):init()
+    --end,
+  },
+  {
+    "Davidyz/VectorCode",
+    version = "*",
+    build = "pipx upgrade vectorcode",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "echasnovski/mini.diff", -- Inline and better diff over the default
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
+  {
+    'mrjones2014/legendary.nvim',
+    -- since legendary.nvim handles all your keymaps/commands,
+    -- its recommended to load legendary.nvim before other plugins
+    priority = 10000,
+    lazy = false,
+    -- sqlite is only needed if you want to use frecency sorting
+    -- dependencies = { 'kkharji/sqlite.lua' }
+  }
+  },
+
+  install = {colorscheme = { "morhetz/gruvbox" } },
+
+  checker = {enabled = true},
+})
